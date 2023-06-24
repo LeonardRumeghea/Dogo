fe9b28148680cbff004c27f0cf185d6c456ba0b6
Leonard Rumeghea <79212407+LeonardRumeghea@users.noreply.github.com>
2023-06-24 15:03:37 +0300

    Update

--- dogo_backend/Dogo.Core/Helpers/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dogo.Core.Helpers
-{
-    public class Config
-    {
-        private const string KEY = "AIzaSyDlvGPPAHeaSX9zsC3FiMHCi3Ix-YFvHVk";
-        public static string GOOGLE_APY_KEY { get => KEY; }
-
-    }
-}
